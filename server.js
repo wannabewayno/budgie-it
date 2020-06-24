c2746d6e75313ab06d5b7f9654fed4fcbaee81b5
Wayne <wayne.c.griffiths@gmail.com>
2020-06-24 12:45:24 +0800

    inital commit ADD code from triology develop folder

---
+++ server.js
@@ -0,0 +1,31 @@
+// Configure environemnt variables
+// ==============================================================================
+require('dotenv').config();
+
+// dependencies
+// ==============================================================================
+const express = require("express");
+const logger = require("morgan");
+const compression = require("compression");
+
+const PORT = 3000;
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(compression());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+// routes
+app.use(require("./routes/api.js"));
+
+
+// set up the db connection and start the server
+// ==============================================================================
+const connection = require('./config/connection');
+connection
+.then( () => app.listen( PORT, () => console.log(`App running on port ${PORT}!`) ) );
